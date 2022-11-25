ff424fc1d12ea6fde4f2c7e30b7064374c627f3d
joao souza <jpnogueira2506@gmail.com>
2022-11-25 10:47:04 -0300

    placar do jogo

--- aula1.js
+++ aula1.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dia = require('./biblioteca');   //const { DiaDaSemana } = require('./biblioteca');
+var dia = require('./biblioteca');  
+//const { DiaDaSemana } = require('./biblioteca');
 http.createServer(function (req,res){
     res.writeHead(200, {'Content-Type':'text/html'})
-    res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
+    res.write('<h2>Brasil 2 x 1 suiça</h2> \n');
     res.write('<p style=color:green>'+Date().substring(16, 24)+'<br>');
     res.write(dia.DiaDaSemana()+'</p><br><br>');
 
