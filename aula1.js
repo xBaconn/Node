8c5afbff6db576e9edccd22be9f3d2c54d54eaeb
joao souza <jpnogueira2506@gmail.com>
2022-11-25 10:16:42 -0300

    versao com hora

--- aula1.js
+++ aula1.js
@@ -1,6 +1,9 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write('Brasil 2 x 0 Sevria \n');
-    res.end('Thaianny linda!');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
+    res.write(Date().substring(16, 24)+'<br>');
+    res.write('gemido da bbw! \n');
+    res.end('niiannnnnnnnnnnnnnnnnnnnnn!');
     
 }).listen(3000);
